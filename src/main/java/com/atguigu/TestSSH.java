fa596e338f93195b4b8aab27924d9d63436465d8
weiyunhui <weiyunhui@atguigu.com>
2024-03-11 15:40:16 +0800

    test

---
+++ src/main/java/com/atguigu/TestSSH.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author WEIYUNHUI
+ * @date 2024/3/11 15:39
+ */
+public class TestSSH {
+    public static void main(String[] args) {
+        System.out.println("TestSSH.main");
+    }
+}
